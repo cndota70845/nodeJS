56c074e6c2ef78d74559daae291e303344b986a7
ils <ly@bjtu.edu.cn>
2021-05-21 17:23:56 +0800

    试接口

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ server.on('request',function (request,response) {
     response.write('hello');
     response.write(`<div><h1>你好</h1></div>`);
     // 告诉客户端，我的话说完了，你可以呈递给用户了
+    console.log(require.extensions);//添加扩展方式，不推荐;
     response.end();
 });
 
--- koa.js
+++ koa.js
@@ -4,50 +4,108 @@ const Koa = require('koa'),
     bodyParser = require('koa-bodyparser'),
     mysql = require('./mysql'),
     path = require('path'),
-    fs = require('fs')
+    fs = require('fs');
 
 var user = undefined;
 
+const static = require('koa-static');
+app.use(static('./static'));
+
 app.use(async (ctx,next)=>{
     console.log(`Process ${ctx.request.method},${ctx.request.url}`);
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     user = await mysql.query();
-    await next();
+    if (ctx.method == 'OPTIONS') {
+        ctx.body = 200; 
+    } 
+    else {
+        await next();
+    }
 });
 
-router.get('/', async (ctx, next) => {
-    var params = '/login.html'
-
-    // var data = fs.readFile(`./static${params}`,(err,data)=>{
-    //     if (err) {
-    //         console.log(err);
-    //         return;
-    //     }
-    //     return data;
-    // });
-    // console.log(data);
-    // ctx.response.status = 200;
-    //     ctx.response.type = 'text/html';
-    //     ctx.response.body = data;
-
-    
+router.get('/login', async (ctx, next) => {
+    var params = '/login.html';
+    ctx.response.status = 200;
+    ctx.response.type = 'text/html';
+    fs.readFile(`./static${params}`,'utf-8',(err,data)=>{
+        if (err) {
+            console.log(err);
+        }
+        else {
+            ctx.response.body = data.toString();
+        }
+    });
+    ctx.response.body = `<div class="box">
+        <h2>Login</h2>
+        <form action="./signin" method="post">
+            <div class="inputBox">
+                <input type="text" name="name" required="">
+                <label>Username</label>
+            </div>
+            <div class="inputBox">
+                <input type="password" name="password" required="" autocomplete>
+                <label>password</label>
+            </div>
+            <input type="submit" name="" value="登录" />
+        </form>
+        <a href="/user">去注册</a>
+    </div>`
     await next();
 });
 
 router.get('/user', async (ctx, next) => {
     ctx.response.body = `<h1>User</h1>
-        <form action="/user" method="post">
-            <p>Name: <input name="name" value=""></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="添加"></p>
-        </form>`;
+    <form action="/user" method="post">
+        <p>Name: <input name="name" value=""></p>
+        <p>Password: <input name="password" type="password"></p>
+        <p><input type="submit" value="添加"></p>
+    </form>`;
+});
+
+router.get('/api/getUser', async (ctx, next) => {
+    function filterUser (query) {
+        return user.filter(function(el) {
+            let choice = true;
+            for (let key in query) {
+                if (el[key] != query[key]) {
+                    choice = false;
+                }
+            }
+            if (choice == true) {
+                return el;
+            }
+        })
+    }
+
+    function killNULL (origin) {
+        let target = {};
+        for (let key in origin) {
+            if (['',undefined,null].includes(origin[key]) === false) {
+                target[key] = origin[key];
+            }
+        }
+        console.log(target);
+        return target;
+    }
+
+    if (ctx.request.query) {
+        let query = killNULL(ctx.request.query);
+        let data = filterUser (query)
+        ctx.body = data;
+    }
+    else {
+        ctx.body = user;
+    }
+    await next();
 });
 
 router.post('/user', async (ctx, next) => {
-    var
-        name = ctx.request.body.name || '',
+    var name = ctx.request.body.name || '',
         password = ctx.request.body.password || '';
         res = await mysql.insert({name:name,password:password});
-    ctx.response.body = `<h1>添加用户，姓名: ${name}, 密码: ${password}</h1>`;
+    ctx.response.body = `<h1>添加用户，姓名: ${name}, 密码: ${password}</h1><br><a href="/login">去登录</a>`;
 });
 
 router.post('/signin', async (ctx, next) => {
@@ -66,7 +124,7 @@ router.post('/signin', async (ctx, next) => {
     }
     else {
         return ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
+        <p><a href="./login">Try again</a></p>`;
     }
 });
 
